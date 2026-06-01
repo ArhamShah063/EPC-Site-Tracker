f1aa381c56d93b2f203c54356c5d1d99674ddcdd
Arham Shah <arham130307@gmail.com>
2026-06-01 17:03:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import seaborn as sns
 import plotly.express as px
 import numpy as np
 
